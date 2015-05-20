2ae163ccca09a1c13a4607b563458abee3193bcf
root <root@SZCLC6X-4763.1234.com>
2015-05-20 17:45:56 +0800

    fenxiao code

---
+++ admin/layout/default.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?php //echo $this->title();?>微信分销后台管理系统</title>
+<?php echo $this->meta();?>
+<?php echo $this->css(array('style.css'));?>
+<?php echo $this->js(array('jquery1.6.js','common.js'));?>
+</head>
+<body>
+<div class="main" style="height:auto; padding-top:1px;">
+	<?php echo $this->content();?>
+</div>
+<div style="clear:both"></div>
+<!--<p align="center"><?php echo base64_decode('5bm/5bee5LiH6Ziz572R57uc56eR5oqA5pyJ6ZmQ5YWs5Y+4');?></p>
+--></body>
+</html>
