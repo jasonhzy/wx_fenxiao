bbbc769a3755d41b17cfd39c1c6d94c78c9afbab
JasonPinet <jason.hu@pinet.co>
2015-06-01 10:25:30 +0800

    Sponsored links

--- m/tpl/15/myerweima.php
+++ m/tpl/15/myerweima.php
@@ -8,7 +8,7 @@
 	快来扫一扫，抢占合伙人地盘！
 	</div>
 	<div style="margin:10%; margin-top:0px;height:45px; line-height:45px; font-size:14px; font-weight:bold; color:#FFF;-webkit-box-shadow: 0px 4px 4px #abaaaa; margin-bottom:5px; line-height:22px">
-	<span style=" display:block; color:#999999">点击复制链接</span>
+	<span style=" display:block; color:#999999">我的推广链接</span>
 	<p class="copyurl" style="width:100%; color:#666; background:#FAFAFA; border:none; overflow:hidden" onclick="clickselect()"><?php echo $thisurl;?></p>
 	</div>
 	<!-- <a href="<?php echo ADMIN_URL;?>user.php?act=ajax_down_img" class="addcar" style=" width:90px; background:#4f82b4; position:fixed; left:5px; bottom:55px;height:24px; line-height:24px; color:#FFF">下载二维码</a> -->
